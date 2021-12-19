dfc2463b47e4786925aa77c8607f696d7b5447c6
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-19 07:31:29 +0000

    first commit

---
+++ lib/st7789_elixir.ex
@@ -0,0 +1,18 @@
+defmodule St7789Elixir do
+  @moduledoc """
+  Documentation for `St7789Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> St7789Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/st7789_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule St7789ElixirTest do
+  use ExUnit.Case
+  doctest St7789Elixir
+
+  test "greets the world" do
+    assert St7789Elixir.hello() == :world
+  end
+end
